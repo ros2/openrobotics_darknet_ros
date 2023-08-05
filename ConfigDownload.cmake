63086f2fae47eab5a9bd2663a2c896e67b9734c3
Tobit Flatscher <53856473+2b-t@users.noreply.github.com>
2023-08-05 15:59:36 +0100

    fix: Apply changes to CMake list and configuration files as requested in pull request

--- ConfigDownload.cmake
+++ ConfigDownload.cmake
@@ -4,33 +4,33 @@
 # See https://pjreddie.com/darknet/yolo/
 file(DOWNLOAD
   https://raw.githubusercontent.com/pjreddie/darknet/master/data/coco.names
-  ${CMAKE_CURRENT_SOURCE_DIR}/config/yolo-v3/coco.names
+  ${CMAKE_CURRENT_BINARY_DIR}/config/yolo-v3/coco.names
   SHOW_PROGRESS
 )
 file(DOWNLOAD
 https://github.com/AlexeyAB/darknet/blob/master/cfg/yolov3.cfg
-  ${CMAKE_CURRENT_SOURCE_DIR}/config/yolo-v3/yolov3.cfg
+  ${CMAKE_CURRENT_BINARY_DIR}/config/yolo-v3/yolov3.cfg
   SHOW_PROGRESS
 )
 file(DOWNLOAD
   https://pjreddie.com/media/files/yolov3.weights
-  ${CMAKE_CURRENT_SOURCE_DIR}/config/yolo-v3/yolov3.weights
+  ${CMAKE_CURRENT_BINARY_DIR}/config/yolo-v3/yolov3.weights
   SHOW_PROGRESS
 )
 # Yolo v3 tiny
 file(DOWNLOAD
   https://raw.githubusercontent.com/pjreddie/darknet/master/data/coco.names
-  ${CMAKE_CURRENT_SOURCE_DIR}/config/yolo-v3-tiny/coco.names
+  ${CMAKE_CURRENT_BINARY_DIR}/config/yolo-v3-tiny/coco.names
   SHOW_PROGRESS
 )
 file(DOWNLOAD
   https://raw.githubusercontent.com/pjreddie/darknet/master/cfg/yolov3-tiny.cfg
-  ${CMAKE_CURRENT_SOURCE_DIR}/config/yolo-v3-tiny/yolov3-tiny.cfg
+  ${CMAKE_CURRENT_BINARY_DIR}/config/yolo-v3-tiny/yolov3-tiny.cfg
   SHOW_PROGRESS
 )
 file(DOWNLOAD
   https://pjreddie.com/media/files/yolov3-tiny.weights
-  ${CMAKE_CURRENT_SOURCE_DIR}/config/yolo-v3-tiny/yolov3-tiny.weights
+  ${CMAKE_CURRENT_BINARY_DIR}/config/yolo-v3-tiny/yolov3-tiny.weights
   SHOW_PROGRESS
 )
 
@@ -38,33 +38,33 @@ file(DOWNLOAD
 # See https://github.com/AlexeyAB/darknet#pre-trained-models
 file(DOWNLOAD
   https://raw.githubusercontent.com/pjreddie/darknet/master/data/coco.names
-  ${CMAKE_CURRENT_SOURCE_DIR}/config/yolo-v4/coco.names
+  ${CMAKE_CURRENT_BINARY_DIR}/config/yolo-v4/coco.names
   SHOW_PROGRESS
 )
 file(DOWNLOAD
 https://raw.githubusercontent.com/AlexeyAB/darknet/master/cfg/yolov4.cfg
-  ${CMAKE_CURRENT_SOURCE_DIR}/config/yolo-v4/yolov4.cfg
+  ${CMAKE_CURRENT_BINARY_DIR}/config/yolo-v4/yolov4.cfg
   SHOW_PROGRESS
 )
 file(DOWNLOAD
   https://github.com/AlexeyAB/darknet/releases/download/darknet_yolo_v3_optimal/yolov4.weights
-  ${CMAKE_CURRENT_SOURCE_DIR}/config/yolo-v4/yolov4.weights
+  ${CMAKE_CURRENT_BINARY_DIR}/config/yolo-v4/yolov4.weights
   SHOW_PROGRESS
 )
 # Yolo v4 tiny
 file(DOWNLOAD
   https://raw.githubusercontent.com/pjreddie/darknet/master/data/coco.names
-  ${CMAKE_CURRENT_SOURCE_DIR}/config/yolo-v4-tiny/coco.names
+  ${CMAKE_CURRENT_BINARY_DIR}/config/yolo-v4-tiny/coco.names
   SHOW_PROGRESS
 )
 file(DOWNLOAD
   https://raw.githubusercontent.com/AlexeyAB/darknet/master/cfg/yolov4-tiny.cfg
-  ${CMAKE_CURRENT_SOURCE_DIR}/config/yolo-v4-tiny/yolov4-tiny.cfg
+  ${CMAKE_CURRENT_BINARY_DIR}/config/yolo-v4-tiny/yolov4-tiny.cfg
   SHOW_PROGRESS
 )
 file(DOWNLOAD
   https://github.com/AlexeyAB/darknet/releases/download/darknet_yolo_v4_pre/yolov4-tiny.weights
-  ${CMAKE_CURRENT_SOURCE_DIR}/config/yolo-v4-tiny/yolov4-tiny.weights
+  ${CMAKE_CURRENT_BINARY_DIR}/config/yolo-v4-tiny/yolov4-tiny.weights
   SHOW_PROGRESS
 )
 
@@ -72,32 +72,34 @@ file(DOWNLOAD
 # See https://github.com/AlexeyAB/darknet/issues/8595
 file(DOWNLOAD
   https://raw.githubusercontent.com/pjreddie/darknet/master/data/coco.names
-  ${CMAKE_CURRENT_SOURCE_DIR}/config/yolo-v7/coco.names
+  ${CMAKE_CURRENT_BINARY_DIR}/config/yolo-v7/coco.names
   SHOW_PROGRESS
 )
 file(DOWNLOAD
   https://raw.githubusercontent.com/AlexeyAB/darknet/master/cfg/yolov7.cfg
-  ${CMAKE_CURRENT_SOURCE_DIR}/config/yolo-v7/yolov7.cfg
+  ${CMAKE_CURRENT_BINARY_DIR}/config/yolo-v7/yolov7.cfg
   SHOW_PROGRESS
 )
 file(DOWNLOAD
   https://github.com/AlexeyAB/darknet/releases/download/yolov4/yolov7.weights
-  ${CMAKE_CURRENT_SOURCE_DIR}/config/yolo-v7/yolov7.weights
+  ${CMAKE_CURRENT_BINARY_DIR}/config/yolo-v7/yolov7.weights
   SHOW_PROGRESS
 )
 # Yolo v7 tiny
 file(DOWNLOAD
   https://raw.githubusercontent.com/pjreddie/darknet/master/data/coco.names
-  ${CMAKE_CURRENT_SOURCE_DIR}/config/yolo-v7-tiny/coco.names
+  ${CMAKE_CURRENT_BINARY_DIR}/config/yolo-v7-tiny/coco.names
   SHOW_PROGRESS
 )
 file(DOWNLOAD
 https://raw.githubusercontent.com/AlexeyAB/darknet/master/cfg/yolov7-tiny.cfg
-  ${CMAKE_CURRENT_SOURCE_DIR}/config/yolo-v7-tiny/yolov7-tiny.cfg
+  ${CMAKE_CURRENT_BINARY_DIR}/config/yolo-v7-tiny/yolov7-tiny.cfg
   SHOW_PROGRESS
 )
 file(DOWNLOAD
   https://github.com/AlexeyAB/darknet/releases/download/yolov4/yolov7-tiny.weights
-  ${CMAKE_CURRENT_SOURCE_DIR}/config/yolo-v7-tiny/yolov7-tiny.weights
+  ${CMAKE_CURRENT_BINARY_DIR}/config/yolo-v7-tiny/yolov7-tiny.weights
   SHOW_PROGRESS
 )
+
+install(DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/config/ DESTINATION share/${PROJECT_NAME}/config/)
